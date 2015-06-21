920b6de2918353dd8fce939106d4f30efde975d6
TravisCG <buldozer88@hotmail.com>
2015-06-21 10:50:53 +0200

    Just save some data

--- server/main.c
+++ server/main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
 
 int main(int argc, char **argv){
-   int s, client;
-   struct sockaddr_rc local = {0}, remote;
+   int s, client, r;
+   struct sockaddr_rc local = {0}, remote = {0};
    socklen_t opt = sizeof(remote);
 
    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+   if(s < 0){
+      printf("Socket error\n");
+      return(EXIT_FAILURE);
+   }
+
    local.rc_family  = AF_BLUETOOTH;
    local.rc_bdaddr  = *BDADDR_ANY;
    local.rc_channel = (uint8_t) 1;
 
-   bind(s, (struct sockaddr*) &local, sizeof(local));
+   r = bind(s, (struct sockaddr*) &local, sizeof(local));
+   if(r < 0){
+      printf("Bind error:%d\n", errno);
+      return(EXIT_FAILURE);
+   }
 
-   listen(s, 1);
+   r = listen(s, 1);
+   if(r < 0){
+      printf("Listen error:%d\n", errno);
+   }
 
    client = accept(s, (struct sockaddr*) &remote, &opt);
 
